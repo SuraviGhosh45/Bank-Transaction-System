94f96cc91b3f51568ea17a0675ee6b4525848b60
Suravi Ghosh <suravighosh45@gmail.com>
2026-05-06 01:38:01 +0530

    server.js updated

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const app=require('./src/app');
+const connectDB=require('./src/config/db');
+require('dotenv').config()
+const dns = require('node:dns'); 
+dns.setServers(["8.8.8.8","1.1.1.1"]);
 
 const port=3000
 
+connectDB();
 app.listen(port,()=>{
     console.log("server running in port ",port);
 })
